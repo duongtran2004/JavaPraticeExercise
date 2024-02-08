62142b5649fbaf523be8ff4920307bdc80a50adf
duongtran2004 <S3979381@rmit.edu.vn>
2024-02-08 18:05:53 +0700

    Add ex2

--- src/duongtran/JavaArrayExercises.java
+++ src/duongtran/JavaArrayExercises.java
@@ -1,5 +1,8 @@
 package duongtran;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class JavaArrayExercises {
@@ -42,8 +45,46 @@ public static void Ex1(){
 
     System.out.println("The average of all element is " + average);
 }
+//Ex1 not yet done error handling
+public static void Ex2(){
+//Write a Java program to sort a numeric array and a string array.
+    //both ascending and descending
+        String[] stringArray = { "Java",
+                "Python",
+                "PHP",
+                "C#",
+                "C Programming",
+                "C++"};
+        int[] numericArray = {4334,33,2,66,78};
+        //sort ascending order
+     Arrays.sort(stringArray);
+        System.out.println("Sorted Ascending stringArray: " + Arrays.toString(stringArray));
+        Arrays.sort(numericArray);
+        System.out.println ("Sorted Ascending numericArray: " + Arrays.toString(numericArray));
+        //sort descending order
+        Arrays.sort(stringArray, Collections.reverseOrder()); //this can be use for String[] as it is object
+    // (reference datatype, but not for primitive datatype like int[]
+        System.out.println("Sorted Descending stringArray: " + Arrays.toString(stringArray));
+    //Implement a custom sorting algorithm for int[] arrays. => Reverse the ascending array
+    //It works by swapping the elements from the beginning and the end of the array until they meet in the middle.
+//Step 1: initialized array length
+    int numericArrayLength = numericArray.length;
+    //Step 2: Loop through the array until reaching the middle
+    for (int i = 0; i< numericArrayLength/2; i++){
+        //temporary variables to swap element
+        int temp = numericArray[i];
+        //swapping values between numericArray[i] and numericArray[numericArrayLength - 1 - i] while keep the middle index the same
+        numericArray[i] //elements from the beginning
+                = numericArray[numericArrayLength -1 - i];
+        numericArray[numericArrayLength -1 - i] //element at the end
+                = temp;
+    }
+        System.out.println("Sorted Descending numericArray: " + Arrays.toString(numericArray));
+    }
+
 
     public static void main(String[] args) {
-        Ex1();
+        //Ex1();
+        Ex2();
     }
 }
