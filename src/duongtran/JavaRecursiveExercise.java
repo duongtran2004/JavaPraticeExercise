89969487834100a3fd0b1f26a9f91e61a7eb9cff
duongtran2004 <S3979381@rmit.edu.vn>
2024-02-09 12:03:56 +0700

    Add ex2 recursion

--- src/duongtran/JavaRecursiveExercise.java
+++ src/duongtran/JavaRecursiveExercise.java
@@ -11,11 +11,25 @@ public class JavaRecursiveExercise {
             return n * factorial(n - 1);
         }
     }
+    public static int sumUpToN(int n){
+        //Requirement: calculate sum of all numbers from 1 to n
+        //base case to exit the loop, prevent stackoverflow
+        if (n == 1){ // sum of all number from 1 to 1 is 1
+        return 1;} else {
+            return n + sumUpToN(n -1); //add everything from n to 1 (descending)
+        }
+    }
+
     public static void main(String[] args) {
 
-//Ex1: Calculate factorial
-        int n = 8;
-        int result = factorial(n);
-        System.out.println("The factorial of " + n + " is " + result);
+////Ex1: Calculate factorial
+//        int n = 8;
+//        int result = factorial(n);
+//        System.out.println("The factorial of " + n + " is " + result);
+//        //end of Ex1
+        //Ex2: Sum of all numbers up to n
+        int n = 9;
+        int result = sumUpToN(n);
+        System.out.println("The sum of all integers from 1 to  " + n + " is " + result);
     }
 }
