f1da759809e9731474f0eece85d48dcc526d36bc
duongtran2004 <S3979381@rmit.edu.vn>
2024-02-20 15:36:07 +0700

    Start doing recursive exercises

--- src/duongtran/methodExercises.java
+++ src/duongtran/methodExercises.java
@@ -1,11 +1,13 @@
 package duongtran;
+//NOTE: Even after you finished an exercise, please review other people's solutions and sample code.
+// They might have a better approach that you can learn to improve your code
 
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class methodExercises {
     public static void main(String[] args) {
-//        //EX1 (Sample Code) Smalles Number
+//        //EX1 (Sample Code) Smallest Number
 //        //main function: user input and display result
 //        Scanner in = new Scanner(System.in);
 //        System.out.print("Input the first number: ");
@@ -28,20 +30,51 @@ public class methodExercises {
 //        //display the results and call the method
 //        System.out.println("The average of 3 numbers is: " + average(a,b,c));
 
-        //EX3: Middle Character
-        //Write a Java method to display the middle character of a string.
-        //Note: a) If the length of the string is odd there will be two middle characters.
-        //b) If the length of the string is even there will be one middle character.
-        //Test Data:
-        //Input a string: 350
-        //Expected Output: 5
+//        //EX3: Middle Character
+//        //Write a Java method to display the middle character of a string.
+//        //Note: a) If the length of the string is odd there will be two middle characters.
+//        //b) If the length of the string is even there will be one middle character.
+//        //Test Data:
+//        //Input a string: 350
+//        //Expected Output: 5
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter a String: ");
+//        String s = scanner.next();
+//        System.out.println("The length of string " + s + " is: " + s.length());
+//        System.out.println("The middle character of string " + s + " is " + middleCharacter(s));
+
+//        //EX4: Count Vowels
+//        //Write a Java method to count all the words in a string.
+//        //Test Data:
+//        //Input the string: The quick brown fox jumps over the lazy dog.
+//        //Expected Output:
+//        //
+//        //Number of words in the string: 9
+//                Scanner scanner = new Scanner(System.in);
+//                System.out.println("Please enter a String: ");
+//                String s = scanner.next();
+//        System.out.println("The numbers of vowels inside string " + s + " is: " + countVowels(s));
+
+        //EX 5: Words count
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter a String: ");
-        String s = scanner.next();
-        System.out.println("The length of string " + s + " is: " + s.length());
-        System.out.println("The middle character of string " + s + " is " + middleCharacter(s));
+        String s = scanner.nextLine(); //let user input a string with spaces
+        System.out.println("The input String \"" + s + "\"" + " has " + wordsCount(s) + " words" );
 
-        //EX4: Count Vowels
+
+//        //EX 6: Sum of digits
+//        //Write a Java method to compute the sum of digits in an integer.
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter an integer: ");
+//        int n = scanner.nextInt();
+//        //display the result
+//        System.out.println("The sum of digits of integer " + n + " is: " + digitSumMethod1(n));
+//        System.out.println("The sum of digits of integer " + n + " is: " + digitSumMethod2(n));
+
+        //EX 7: Pentagonal Numbers Sequence (can be done through recursive)
+
+        //EX 8: Calculate investment value given interest rate (can be done through recursive)
+
 
     }
 
@@ -57,21 +90,100 @@ public class methodExercises {
         return (a + b + c) / 3;
     }
 
+    //EX3
     public static String middleCharacter(String s) {
+        //must convert to String since char cannot hold 2 characters (even length case)
         String c = Character.toString(s.charAt(s.length() / 2));
         String d = "";
         String e;
-
-
-        if (s.length() % 2 == 0) { //even String length: 2 middle character
+        //odd String length: 1 middle character.(default case, outside the if conditional
+//even String length: 2 middle character
+        if (s.length() % 2 == 0) {
             d = Character.toString(s.charAt(s.length() / 2 - 1));
 
         }
-        //odd String length: 1 middle character
 
-        System.out.println("d : " + d);
+
         return e = d + c;
     }
+
+    //EX4
+    public static int countVowels(String s) {
+        //initialize counting variable
+        int totalVowels = 0;
+        //store String's characters into array elements
+        char[] charArray = s.toCharArray();
+
+        //loop through the array (for loop)
+        for (int i = 0; i < charArray.length; i++) {
+            //if condition to update totalVowels +=
+            if (charArray[i] == 'a' || charArray[i] == 'e' || charArray[i] == 'i' || charArray[i] == 'o' || charArray[i] == 'u') { //Vowels: a, e, i, o, u
+                totalVowels += 1;
+            }
+        }
+
+        return totalVowels;
+
+    }
+
+    //Ex5
+    public static int wordsCount(String s){
+        //initialize counting variable
+        int count = 0;
+        //1st approach:
+        //Step 1: Split string by space " " => turn into new substring
+        // Step 2: store those new substrings as element of a String [] stringArray
+        String[] stringArray = s.split(" ");
+        //troubleshoot
+        // Step 3: Loop through array and update the count
+        for (int i = 0; i < stringArray.length; i ++){
+            count += 1;
+        }
+        return count;
+    }
+
+    //EX6
+    public static int digitSumMethod1(int n) {
+        //initialize counting variable
+        int sum = 0;
+        //convert integer into string
+        String stringInt = Integer.toString(n);
+        //store string character into an array
+        char[] charArray = stringInt.toCharArray();
+        //loop through array
+        for (int i = 0; i < charArray.length; i++) {
+            //*convert each element of array from char to int (getNumericValue)
+            int a = Character.getNumericValue(charArray[i]);
+            //update counting variable
+            sum += a;
+        }
+
+        return sum;
+    }
+    public static int digitSumMethod2( int n){
+        //initialize counting variable
+        int sum = 0;
+        //extract the last digits of n from the right with remainder division by 10, then add to the result for update
+        //move the decimal places 1 place toward left by integer division by 10 then assign new division result to n
+        //repeat the same process until all digits has been extracted and added to sum
+        while (n > 0){ //condition to escape the loop: when all the digits has been extracted,
+            // n only has 1 digit left, then n / 10 (integer division) = 0 (as n < 10),
+            // then assign division result (0) to n. Now n = 0, which does not satisfy the condition (n must > 0)
+            sum += n % 10;
+            n /= 10;
+
+        }
+        return  sum;
+    }
+
+    //EX7
+    public static int[] pentagonalNumbersSequence(int n){ //this can be solved using recursive
+        //initialize array variable to hold numbers. "n" in the parameter is the term of sequence
+        int []pentagonalNumbersSequence = {};
+
+        return pentagonalNumbersSequence;
+    }
+
 
 }
 
--- src/duongtran/recursiveExercise.java
+++ src/duongtran/recursiveExercise.java
@@ -1,97 +1,39 @@
 package duongtran;
 
+import java.util.Scanner;
+
 public class recursiveExercise {
-    public static int factorial(int n) { //pass int n to method parameter
-        //Requirement: Factorial
-        //base case to exit the loop, prevent stackoverflow
-        if (n == 1 || n == 0) { //factorial of 1 or 0 is 1
-            return 1;
-        } else {
-            //what you want the recursive to do
-            return n * factorial(n - 1);
+    //EX 1:
+    public static int calculateFactorial(int n) {
+        int factorialProduct = 0;
+        //base case to exit the recursive from keep calling itself
+        // factorial of 0 is 1
+        if (n == 0) {
+            return 1; //the code below would be 1
         }
-    }
-    public static int sumUpToN(int n){
-        //Requirement: calculate sum of all numbers from 1 to n
-        //base case to exit the loop, prevent stackoverflow
-        if (n == 1){ // sum of all number from 1 to 1 is 1
-        return 1;} else {
-            return n + sumUpToN(n -1); //add everything from n to 1 (descending)
-        }
-    }
-    public static int Fibonacci(int n){
-        //Requirement: given the nth term, calculate the Fibonacci numbers (print all of them)
-        //base case to exit the loop, prevent stackoverflow
-        if (n == 1 ){
-            return 0; // The 1st term of Fibonacci sequences is 0
-        } else if (n == 2) {
-            return 1; // The 2nd term of Fibonacci sequences is 1
-        }
-        else {
-            //In Fibonacci Sequences, a number is sum of its previous 2 term
-            return Fibonacci(n -1 ) + Fibonacci(n-2);
-        }
-        //Find Fibonacci number
-        // print all fibonnaci sequences
-        // sum of all fibonacci squences
-        // give number, find index
-    }
 
+        System.out.println("Calculating factorial(" + n + ")");
+        //call itself. Remember moving toward base case
+        return n * calculateFactorial(n - 1);
 
-
+    }
+    //EX 2:
+    public static int sumFrom1toN(int n){
+        return 1;
+    }
+    public static int sumFromAtoB(int n){
+        return 1;
+    }
 
     public static void main(String[] args) {
+        //EX1: Calculate Factorial
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer: ");
+        int n = scanner.nextInt();
+        System.out.println("The factorial of " + n + " is " + calculateFactorial(n));
 
-////Ex1: Calculate factorial
-//        int n = 8;
-//        int result = factorial(n);
-//        System.out.println("The factorial of " + n + " is " + result);
-//        //end of Ex1
-//        //Ex2: Sum of all numbers up to n
-//        int n = 9;
-//        int result = sumUpToN(n);
-//        System.out.println("The sum of all integers from 1 to  " + n + " is " + result);
-//        //end of Ex1
-
-//        Start of Ex2
-//        //Ex2: Printing Fibonacci Sequences's number's value up to Nth term
-//        int n = 20;
-//       int result = Fibonacci(n);
-//
-//
-//       // now improve its functionalities: return the correct term string: 1st, 2nd, 3rd, or th
-//        //Note for special case: 11 (th), 12 (th), 13(th) => required Second Last Digits
-//        //Step 1: Convert int n to String
-//        String convertedString = Integer.toString(n);
-//        //Step 2: Get the last element from converted String (last digit)
-//        String lastDigits = convertedString.substring(convertedString.length() - 1);
-//        //Step 3: Get the second-last element from converted String (second-last digit)
-//        String secondLastDigits = "0"; //default = 0 if string length is 1
-//        if (convertedString.length() >= 2){ //if string length is larger than 1, filt out special case like 11,12,13 (all th)
-//            // by getting the second last digit
-//     secondLastDigits = convertedString.substring(convertedString.length() - 2, convertedString.length() - 1);}
-//        //Step 4: Conditions to evaluate last digits: 1st, 2nd, 3rd, special case (11,12,13) and th
-//        if (lastDigits.equals("1") && !secondLastDigits.equals("1")){ //note that please compare lastDigits with String Literal "1" instead of integer 1
-//            // as .equals() require both value and datatype to be the same in order to be correct.
-//            //Otherwise, it would be false and pass the conditions to the next else if block
-//            System.out.println("The Fibonacci sequences up to " + n + "st term is " + result);
-//        } else if (lastDigits.equals("2") && !secondLastDigits.equals("1")) {
-//            System.out.println("The Fibonacci sequences up to " + n + "nd term is " + result);
-//        } else if (lastDigits.equals("3")  && !secondLastDigits.equals("1'")){
-//            System.out.println("The Fibonacci sequences up to " + n + "rd term is " + result);
-//        }
-//        else { System.out.println("The Fibonacci sequences up to " + n + "th term is " + result);
-//           ;
-//        }
-//        //end of Ex2
-
-//        //Start of Ex3
-
-//        //End of Ex3
-
-        //Start of Ex4
-        //Exercise 4: Print the Fibonacci Sum up th Nth Term
-// End of Ex4
+        //EX2:
 
     }
 }
+
