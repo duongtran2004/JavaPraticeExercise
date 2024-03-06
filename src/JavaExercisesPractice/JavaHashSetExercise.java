98fa6deedd714c4e0355792e1b280fa832f6e68f
duongtran2004 <S3979381@rmit.edu.vn>
2024-03-06 19:39:25 +0700

    Update 8 more exercises in HashSet

--- src/JavaExercisesPractice/JavaHashSetExercise.java
+++ src/JavaExercisesPractice/JavaHashSetExercise.java
@@ -1,8 +1,6 @@
 package JavaExercisesPractice;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author Tran Dang Duong
@@ -12,40 +10,73 @@ import java.util.Scanner;
  * @since ${11.0.18}
  */
 public class JavaHashSetExercise {
-    public static void printAllElementUsingForEach(HashSet<Integer> hashSet){
-        for (Integer n: hashSet
-             ) {
+    public static void printAllElementUsingForEach(HashSet<Integer> hashSet) {
+        for (Integer n : hashSet
+        ) {
             System.out.println(n);
         }
 
     }
-    public static void printAllElementUsingIterator(HashSet<Integer> hashSet){
+
+    public static void printAllElementUsingIterator(HashSet<Integer> hashSet) {
         //call iterator object
         Iterator<Integer> iterator = hashSet.iterator();
         //iterate the hash set using while loop
-        while (iterator.hasNext()){ //next element sill exist
+        while (iterator.hasNext()) { //next element sill exist
             System.out.println(iterator.next());
         }
 
     }
-    public static void deleteAllElement(HashSet<Integer> hashSet){
+
+    public static void deleteAllElement(HashSet<Integer> hashSet) {
         hashSet.removeAll(hashSet);
         System.out.println("New HashSet is: " + hashSet);
     }
 
-    public static void countHashSetElements(HashSet<Integer> hashSet){
+    public static void countHashSetElements(HashSet<Integer> hashSet) {
         int count = 0;
-        for (Integer n: hashSet
-             ) {
-            count ++;
+        for (Integer n : hashSet
+        ) {
+            count++;
         }
         System.out.println("Total number of element in the HashSet is: " + count);
     }
 
+    public static void checkForEmptyHashSet(HashSet hashSet) {
+        //call iterator object
+        Iterator<Integer> iterator = hashSet.iterator();
+        if (iterator.hasNext() == false) {
+            System.out.println("The HashSet is  empty !");
+        } else {
+            System.out.println("The HashSet is not empty !");
+        }
+    }
+
+    public static void cloneHashSet(HashSet hashSet) {
+        HashSet<Integer> newHashSet = (HashSet<Integer>) hashSet.clone();
+        ;
+
+        System.out.println("New HashSet is: " + newHashSet);
+
+    }
+
+    public static void convertHashSetToArray(HashSet hashSet) {
+        Object[] newArray = hashSet.toArray();
+        System.out.println("New Array is: " + Arrays.toString(newArray));
+
+
+    }
+
+    public static void convertHashSetToTreeSet(HashSet hashSet) {
+        TreeSet<Integer> newTreeSet = hashSet.t
+        System.out.println("New Array is: " + newTreeSet);
+
+
+    }
 
     public static void main(String[] args) {
         //create a HashSet, remember to use Wrapper Class to convert integer to Object and pass to HashSet parameters.
-        HashSet<Integer>integerNumbers = new HashSet<Integer>(10);
+        HashSet<Integer> integerNumbers = new HashSet<Integer>(10);
         //add elements into HashSet
         integerNumbers.add(7);
         integerNumbers.add(0);
@@ -58,11 +89,12 @@ public class JavaHashSetExercise {
 
         //Call methods
 //printAllElementUsingForEach(integerNumbers);
-       // printAllElementUsingIterator(integerNumbers);
+        // printAllElementUsingIterator(integerNumbers);
         //deleteAllElement(integerNumbers);
-        countHashSetElements(integerNumbers);
-
-
+        //countHashSetElements(integerNumbers);
+        //checkForEmptyHashSet(integerNumbers);
+        //cloneHashSet(integerNumbers);
+        convertHashSetToArray(integerNumbers);
 
 
     }
