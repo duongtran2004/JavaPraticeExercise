16d43dbac3ed0fbbb431d83bc2c9894963ae16ed
duongtran2004 <S3979381@rmit.edu.vn>
2024-04-04 11:01:39 +0700

    Start doing some Random Access File Exercise

---
+++ src/JavaRandomAcessFileExercise.java
@@ -0,0 +1,10 @@
+/**
+ * @author Tran Dang Duong
+ * Student ID: s3979381
+ * @version ${11.0.18}
+ * @created 04-Apr-24 11:01 AM
+ * @project JavaPracticeExercises.java
+ * @since ${11.0.18}
+ */
+public class JavaRandomAcessFileExercise {
+}
